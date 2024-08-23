c6e40540eeb99f576655508bafe66c9bfc4f2fcb
Dimitri <dimitri.timoz@orange.fr>
2024-08-23 21:15:35 +0200

    Init web-app

---
+++ web-app/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <ul>
+                <li><nuxt-link to="/">Home</nuxt-link></li>
+                <li v-if="!authenticated" class="loginBtn" style="float: right">
+                    <nuxt-link to="/login">Login</nuxt-link>
+                </li>
+            </ul>
+        </header>
+        <div class="mainContent">
+            <slot />
+        </div>
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+    </div>
+</template>
