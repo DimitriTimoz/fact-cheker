7eaa291f5c7e04376f35c31641e6db05e654a1cd
Dimitri <dimitri.timoz@orange.fr>
2024-08-23 22:57:20 +0200

    Set layout

--- web-app/layouts/default.vue
+++ web-app/layouts/default.vue
@@ -1,18 +1,24 @@
 <template>
-    <div>
-        <header>
-            <ul>
-                <li><nuxt-link to="/">Home</nuxt-link></li>
-                <li v-if="!authenticated" class="loginBtn" style="float: right">
-                    <nuxt-link to="/login">Login</nuxt-link>
-                </li>
-            </ul>
-        </header>
-        <div class="mainContent">
+    <header class="bg-background text-text border-b border-primary">
+        <nav class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl" aria-label="Global">
+            <div class="flex items-center ml-2">
+                <nuxt-link to="/">Home</nuxt-link>
+            </div>
+            <div class="flex items-center" v-if="!authenticated" >
+                <nuxt-link to="/login" class="text-text  hover:primary focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Login</nuxt-link>
+            </div>
+        </nav>
+    </header>
+    <div class="mainContent">
             <slot />
         </div>
         <footer>
             <h1>Footer</h1>
         </footer>
-    </div>
+
 </template>
+<style>
+    .btn {
+        @apply text-text;
+    }
+</style>
