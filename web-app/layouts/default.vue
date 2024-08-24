518c121f8b275aac8a62fa9419da1f9bf2f0178c
Dimitri <dimitri.timoz@orange.fr>
2024-08-24 17:22:52 +0200

    Add animation on hover

--- web-app/layouts/default.vue
+++ web-app/layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
     <header class="bg-background text-text border-b border-primary">
         <nav class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl" aria-label="Global">
-            <div class="flex items-center ml-2">
+            <div class="flex items-center ml-2 text-lg">
                 <nuxt-link to="/">Home</nuxt-link>
             </div>
             <div class="flex items-center" v-if="!authenticated" >
-                <nuxt-link to="/login" class="text-text  hover:primary focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Login</nuxt-link>
+                <nuxt-link to="/login" class="text-text hover:scale-110 transition-all font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Login</nuxt-link>
             </div>
         </nav>
     </header>
