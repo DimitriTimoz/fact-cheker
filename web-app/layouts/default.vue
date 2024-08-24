2b0da077e2688928d321e9c27dbdd8cae28807c0
Dimitri <dimitri.timoz@orange.fr>
2024-08-24 18:15:39 +0200

    Add footer

--- web-app/layouts/default.vue
+++ web-app/layouts/default.vue
@@ -1,19 +1,24 @@
 <template>
-    <header class="bg-background text-text border-b border-primary">
-        <nav class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl" aria-label="Global">
-            <div class="flex items-center ml-2 text-lg">
-                <nuxt-link to="/">Home</nuxt-link>
+    <div class="flex flex-col h-screen justify-between">
+        <header class="bg-background text-text border-b border-primary">
+            <nav class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl" aria-label="Global">
+                <div class="flex items-center ml-2 text-lg">
+                    <nuxt-link to="/">Home</nuxt-link>
+                </div>
+                <div class="flex items-center" v-if="!authenticated" >
+                    <nuxt-link to="/login" class="text-text hover:scale-110 transition-all font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Login</nuxt-link>
+                </div>
+            </nav>
+        </header>
+        <main class="mainContent mb-auto">
+            <slot />
+        </main>
+        <footer class="bg-background text-text border-t border-primary">
+            <div class="flex justify-center items-center mx-auto max-w-screen-xl">
+                <p class="text-center">Dimitri Timoz © 2024</p>
             </div>
-            <div class="flex items-center" v-if="!authenticated" >
-                <nuxt-link to="/login" class="text-text hover:scale-110 transition-all font-medium rounded-lg text-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Login</nuxt-link>
-            </div>
-        </nav>
-    </header>
-    <div class="mainContent">
-        <slot />
+        </footer>
     </div>
-    <footer>
-    </footer>
 </template>
 <style>
 </style>
