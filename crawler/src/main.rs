20019b0e7dd3e45199a8cc01e71ceee996e419a0
Dimitri <dimitri.timoz@orange.fr>
2024-08-26 23:05:09 +0200

    Add logging

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,4 +1,5 @@
 extern crate spider;
+extern crate env_logger;
 
 use std::hash::{DefaultHasher, Hash, Hasher};
 
@@ -6,6 +7,7 @@ use futures::executor::block_on;
 use newspaper::{Newspaper, NewspaperModel, Paper};
 use spider::website::Website;
 use spider::tokio;
+use env_logger::Env;
 
 use meilisearch_sdk::client::*;
   
@@ -14,15 +16,21 @@ pub mod newspaper;
 
 #[tokio::main]
 async fn main() {
+    let env = Env::default()
+    .filter_or("RUST_LOG", "warn")
+    .write_style_or("RUST_LOG_STYLE", "always");
+
+    env_logger::init_from_env(env);
+
     let file = std::fs::File::open("newspapers.json").unwrap();
     let newspapers: Vec<NewspaperModel> = serde_json::from_reader(file).unwrap();
 
     let newspapers = newspapers.iter().map(|newspaper| Newspaper::from(newspaper.clone())).collect::<Vec<_>>();
     let mut hasher: DefaultHasher = DefaultHasher::new();
-
+    // TODO: Use a thread pool to scrape multiple websites concurrently
     for paper in newspapers {
        let mut website = Website::new(paper.get_url().as_str());
-        website.with_limit(1000);
+        website.with_limit(10000);
         println!("Scraping {}", paper.get_title());
         website.scrape().await;
         println!("Scraping done");
