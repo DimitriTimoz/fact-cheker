5064b4439eedfbc12d69253ff04b64b729ecc752
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-09-01 22:52:14 +0200

    Back to classic server runner

--- factcheker/start.sh
+++ factcheker/start.sh
@@ -1,4 +1,5 @@
 python manage.py makemigrations
 python manage.py migrate
 export DEBUG=False
-gunicorn conf.wsgi:application --workers 3
+# gunicorn conf.wsgi:application --workers 3
+python manage.py runserver
