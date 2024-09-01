4c020fa7fcf634e176062327886f2ab57e0a6801
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-09-01 15:50:19 +0200

    Add description

--- web-app/pages/check.vue
+++ web-app/pages/check.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="container lg mx-auto center max-w-screen-lg p-2">
+        <h1 class="text-title font-bold text-2xl">Fact Checker</h1>
+        <p> Enter a statement to find relevant reviews and references to verify the statement.</p>
         <textarea class="text-text bg-transparent center w-full mt-5 p-2 outline-blue-500 border-2 border-primary rounded-lg mb-2"
                     placeholder="The statement..."
                     v-model="value">
