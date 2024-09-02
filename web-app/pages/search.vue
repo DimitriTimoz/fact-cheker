c8a060494879d669fb4b8e2e93fa2477238663e5
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-09-02 21:11:32 +0200

    web-app(add): See results

--- web-app/pages/search.vue
+++ web-app/pages/search.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class="container flex flex-col items-center mt-4">
+    <div class="container flex mx-auto max-w-6xl flex-col items-center mt-4">
         <h2 class="text-text font-bold text-2xl m-6">Search among newspapers</h2>
-        <div class="rounded-2xl w-4/5">
+        <div class="flex rounded-2xl w-4/5">
             <input type="text" v-model="search" placeholder="Search for a newspaper" 
-                class="text-input" />
+                class="text-input w-full h-14" />
             <button @click="searchNewspaper" class="btn">Search</button>    
+        </div>
+        <div v-if="message" class="mt-5 alert" role="alert">
+            <span>{{ message }}</span>
+        </div>
+        <div v-if="newspapers" class="mt-5 container list-none">
+            <ul class="column flex-row border-2 rounded-md p-2 shadow-sm">
+                <li v-for="newspaper in newspapers" class="mt-5">
+                    <div class="flex flew-row border-b">
+                        <a :href="newspaper.url" target="_blank" class="text-text">{{newspaper.title}}</a>
+                    </div>
+                    <p class="text-text" >{{ newspaper.description }}</p>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -12,6 +25,15 @@
 import { $apifetch } from '~/composable/fetch';
 
 const search = ref('');
+const message = ref('');
+
+const newspapers = ref([] as Newspaper[]);
+
+interface Newspaper {
+    title: string;
+    url: string;
+    description: string;
+}
 
 const searchNewspaper = async () => {
     try {
@@ -21,9 +43,21 @@ const searchNewspaper = async () => {
                 q: search.value
             }
         });
-        console.log(response);
+
+        newspapers.value = response;
+        console.log(newspapers.value);
+        message.value = "";
     } catch (error: any) {
-        console.error(error);
+        // Check if the error is a string
+        if (typeof error === 'string') {
+            message.value = error;
+            return;
+        }
+
+        // Check if the error is an object
+        if (error.data && error.data.error) {
+            message.value = error.data.error;
+        }
     }
 }
 
